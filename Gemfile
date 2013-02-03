057e026a9fee186e8406a967f2fd4fd67d02ed19
Mukund Lakshman <yaymukund@gmail.com>
2013-02-02 21:33:19 -0800

    Skeletal commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source :rubygems
+
+gem 'rails', '3.2.11'
+gem 'sqlite3'
+gem 'strong_parameters'
+
+group :assets do
+  gem 'sass-rails', '~> 3.2.3'
+  gem 'haml-rails'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test, :development do
+  gem 'rspec-rails', '~> 2.0'
+  gem 'fabrication'
+end
+
+# To use ActiveModel has_secure_password
+gem 'bcrypt-ruby', '~> 3.0.0'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = '84b81100014e7a513cc63b0e220c36e872610b58ea72b4ae7996c463af1a2ac97720cbe34b366e661ef86c590736038259ab2dffeecdf9f06ed1b7a4a583805e'
